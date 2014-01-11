6cdb7f37096ef0e2c5651b0f2aec6fd40b072cfd
Sean Freiburg <frycicle@gmail.com>
2014-01-11 15:38:52 -0600

    added capistrano

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
   gem 'annotate', '2.5.0'
+  gem 'capistrano'
 end
 
 # Gems used only for assets and not required
---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+require "bundler/capistrano"
+#load 'lib/deploy/seed' #include if you need to load seed data with cap deploy:seed
+
+set :stages, %w(develop production ci)
+set :default_stage, "develop"
+require 'capistrano/ext/multistage'
+server "seanfreiburg.com", :app, :web, :db, :primary => true
+set :user, "root" # The server's user for deploys
+set :scm_passphrase, "emagxgestqkf" # The deploy user's password
+
+set :application, "sean"
+
+set :use_sudo, false
+
+default_environment["GEM_PATH"] ="/home/deployer/.rbenv/versions/1.9.3-p385/lib/ruby/gems/1.9.1:/home/deployer/.rbenv/shims/ruby"
+default_environment["PATH"] = "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$HOME/.rbenv/versions/1.9.3-p385/lib/ruby/gems/1.9.1:$PATH"
+
+set :deploy_via, :remote_cache
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy:update_code", "deploy:migrate"
+after "deploy:cleanup", "deploy:seed"
+
+set :scm, "git"
+set :scm_verbose, true
+set :repository, "git@github.com:seanfreiburg/sean.git"
+
+
+default_run_options[:pty] = true # Must be set for the password prompt from git to work
+ssh_options[:forward_agent] = true
+
+namespace :deploy do
+# If you need to load seed data. Syntax: cap deploy:seed
+  desc "Reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  task :start do ; end
+  task :stop do ; end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}" #restarts nginx
+  end
+end
+
