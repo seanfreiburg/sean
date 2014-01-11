7ba7272b16f925e43a8be9e68178f5114367fb2a
Sean Freiburg <frycicle@gmail.com>
2014-01-11 13:53:29 -0600

    removed cruft

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @projects1 = Project.last(3).reverse!
-    @projects2 = Project.offset(3).last(3).reverse!
   end
 
   def help
--- app/models/project.rb
+++
@@ -1,3 +0,0 @@
-class Project < ActiveRecord::Base
-  attr_accessible :description, :link, :name, :picture_link
-end
