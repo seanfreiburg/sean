1b7f1a31e34c500a3c89bb9237580606d44bff9c
Sean Freiburg <frycicle@gmail.com>
2014-03-27 14:20:22 -0500

    updated contact me

--- public_html/contact.php
+++ public_html/contact.php
@@ -2,8 +2,35 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-6">
-      <h1>Add to this</h1>
+    <div class="col-md-6 col-md-offset-3">
+      <h1>Contact me</h1>
+      <h4>You can email me at seanfreiburg@gmail.com or at</h4>
+      <div class="row">
+        <div class="col-md-2">
+          <a href="https://github.com/seanfreiburg">
+            <img src="assets/img/github-icon.png" class="img-responsive">
+          </a>
+
+        </div>
+        <div class="col-md-2">
+          <a href="https://plus.google.com/u/0/106620845984762511462/">
+            <img src="assets/img/google-icon.png" class="img-responsive">
+          </a>
+
+        </div>
+        <div class="col-md-2">
+          <a href="http://www.linkedin.com/pub/sean-freiburg/26/971/b75">
+            <img src="assets/img/linkedin-icon.png" class="img-responsive">
+          </a>
+
+        </div>
+        <div class="col-md-2">
+          <a href="https://twitter.com/SFreiburg">
+            <img src="assets/img/twitter-icon.png" class="img-responsive">
+          </a>
+
+        </div>
+      </div>
     </div>
   </div>
 </div>
