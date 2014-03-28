9254e8ae1f73d3ace177b7ea6601fd268662afd6
Sean Freiburg <frycicle@gmail.com>
2014-03-27 19:01:40 -0500

    back to md on index

--- public_html/index.php
+++ public_html/index.php
@@ -2,36 +2,36 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-sm-6 text-center">
+    <div class="col-md-6 text-center">
       <h1>I am Sean Freiburg</h1>
 
       <h3>Computer Science major at University of Illinois Urbana-Champaign. Full stack developer. Starting at Microsoft
         this summer. </h3>
       <hr>
       <div class="row">
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <a href="contact.php">
             <button class="btn btn-default btn-lg btn-block">contact me</button>
           </a>
         </div>
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <a href="https://github.com/seanfreiburg">
             <button class="btn btn-default btn-lg btn-block">github</button>
           </a>
         </div>
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <a href="projects.php">
             <button class="btn btn-default  btn-lg btn-block">projects</button>
           </a>
         </div>
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <a href="about.php">
             <button class="btn btn-default  btn-lg btn-block">resume</button>
           </a>
         </div>
       </div>
     </div>
-    <div class="col-sm-6">
+    <div class="col-md-6">
       <img src="assets/img/sean.jpg" class="img-responsive img-rounded center" height="400px" width="400px">
     </div>
   </div>
